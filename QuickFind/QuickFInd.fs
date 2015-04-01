11e34a2f90da8869612910047ea4883d43cd33d6
Mihai Veres <veres85@yahoo.com>
2015-04-01 22:47:09 +0300

    quick find F# implementation 1st commit
    quick find and union find

---
+++ QuickFind/QuickFInd.fs
@@ -0,0 +1,53 @@
+﻿namespace impl
+
+module QuickFind =
+
+    let areConnected (id: array<int>)  i j =
+        id.[i] = id.[j]
+
+    let connect (id: array<int>)  i j =
+        let iVal = id.[i]
+        let jVal = id.[j]
+        id |> Array.map (fun e -> if e = iVal then jVal else e)
+
+
+module UnionFind =
+
+    let rec getRoot (id: array<int>) i =
+        if id.[i] = i then i else getRoot id id.[i]
+
+    let areConnected (id: array<int>)  i j =
+        getRoot id i = getRoot id j
+
+    let union (id: array<int>)  i j =
+        let ri = getRoot id i
+        let rj = getRoot id j
+        Array.set id ri rj
+        id
+
+
+module WeightedUnionFind =
+
+    let initialize n =
+        [|0..n-1|], Array.create n 1
+
+    let rec getRoot (id: array<int>) i =
+        if id.[i] = i then i else getRoot id id.[i]
+
+    let areConnected (id: array<int>)  i j =
+        getRoot id i = getRoot id j
+
+    let replace ri rj (id: array<int>) (sz: array<int>) =
+        Array.set id ri rj
+        Array.set sz rj (sz.[rj] + sz.[ri])
+        id, sz
+
+    let union i j (id: array<int>, sz: array<int>) =
+        let ri = getRoot id i
+        let rj = getRoot id j
+
+        if ri = rj 
+        then id, sz
+        elif sz.[ri] < sz.[rj]
+        then replace ri rj id sz
+        else replace rj ri id sz
---
+++ QuickFind/QuickFindTests.fs
@@ -0,0 +1,49 @@
+﻿module QuickFindTests
+
+open NUnit.Framework
+open FsUnit
+open impl.QuickFind
+open impl.UnionFind
+
+
+[<Test>]
+let ``given [|1; 1; 2|] expect 0 and 1 are connected`` () =
+    impl.QuickFind.areConnected [|1; 1; 2|] 0 1 |> should be True
+
+
+[<Test>]
+let ``when connect [|0; 1; 2|] 0 1 expect [|1; 1; 2|] `` () =
+    impl.QuickFind.connect [|0; 1; 2|] 0 1 |> should equal [|1; 1; 2|]
+
+
+[<Test>]
+let ``given [|0; 1; 3; 5; 4; 4|] expect root of 2 is 4`` () =
+    impl.UnionFind.getRoot [|0; 1; 3; 5; 4; 4|] 2 |> should equal 4
+
+[<Test>]
+let ``given [|0; 1; 3; 5; 4; 4|] expect 2 and 3 are connected`` () =
+    impl.UnionFind.areConnected [|0; 1; 3; 5; 4; 4|] 2 3 |> should be True
+
+
+[<Test>]
+let ``when union [|0; 1; 3; 5; 4; 4|] 1 2 expect [|0; 4; 3; 5; 4; 4|]`` () =
+    impl.UnionFind.union [|0; 1; 3; 5; 4; 4|] 1 2 |> should equal [|0; 4; 3; 5; 4; 4|]
+
+
+[<Test>]
+let ``many weighted unions`` () =
+    let id0, sz0 = impl.WeightedUnionFind.initialize 10
+    let union = impl.WeightedUnionFind.union
+    let id1, sz1 =
+        (id0, sz0)
+        |> union 4 3
+        |> union 3 8
+        |> union 6 5
+        |> union 9 4
+        |> union 2 1
+        |> union 5 0
+        |> union 7 2
+        |> union 6 1
+        |> union 7 3
+    id1 |> should equal [|6; 2; 6; 4; 6; 6; 6; 2; 4; 4|]
+
